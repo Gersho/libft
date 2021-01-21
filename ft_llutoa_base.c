e79ec4463454909aac509da2969a0c6f85d42bf9
Zennoune Karim <kzennoun@student.42lyon.fr>
2021-01-21 12:44:48 +0100

    norm fix

--- ft_llutoa_base.c
+++ ft_llutoa_base.c
@@ -6,18 +6,18 @@
 /*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/20 16:28:26 by kzennoun          #+#    #+#             */
-/*   Updated: 2021/01/20 16:42:47 by kzennoun         ###   ########lyon.fr   */
+/*   Updated: 2021/01/21 12:40:55 by kzennoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "libft.h"
 
-int		ft_get_digit_count(unsigned long long int nbr, int base_len)
+static int	ft_get_digit_count(unsigned long long int nbr, int base_len)
 {
 	int		i;
 
 	i = 1;
-	while (nbr >= base_len)
+	while (nbr >= (unsigned)base_len)
 	{
 		i++;
 		nbr /= base_len;
@@ -25,7 +25,7 @@ int		ft_get_digit_count(unsigned long long int nbr, int base_len)
 	return (i);
 }
 
-int		check_base_is_valid(char *base)
+int			check_base_is_valid(char *base)
 {
 	int i;
 	int j;
@@ -47,29 +47,23 @@ int		check_base_is_valid(char *base)
 	return (1);
 }
 
-char		*ft_lltuoa_base(unsigned long long int nbr, char *base)
+char		*ft_llutoa_base(unsigned long long int nbr, char *base)
 {
 	int			base_len;
 	int			digit_count;
+	char		*str;
 
-
-	// malloc selon la taille de ft_get_digit_count(unsigned long long int nbr, int base_len)
-	//boucler avec un while basé sur digitcount
 	base_len = ft_strlen(base);
 	digit_count = ft_get_digit_count(nbr, base_len);
 	if (!check_base_is_valid(base) || base_len < 2)
-		return ;
-	while (nbr >= 0)
+		return (NULL);
+	if (!(str = malloc(sizeof(char) * (digit_count + 1))))
+		return (NULL);
+	while (digit_count >= 0)
 	{
-		if (nbr < base_len)
-		{
-			ft_putchar(base[nbr]);
-		}
-		else
-		{
-			ft_putnbr_base(nbr / base_len, base);
-			ft_putnbr_base(nbr % base_len, base);
-		}
-		nbr = -1;
+		str[digit_count - 1] = base[nbr % base_len];
+		nbr /= base_len;
+		digit_count--;
 	}
+	return (str);
 }
