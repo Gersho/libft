00a04f94165349d18258c9387d6876144e391762
Zennoune Karim <kzennoun@student.42lyon.fr>
2021-02-19 13:10:22 +0100

    norming done

--- Makefile
+++ Makefile
@@ -1,24 +1,21 @@
-SRCS	= ft_isalnum.c ft_memccpy.c ft_memset.c ft_strncmp.c ft_strchr.c ft_isalpha.c \
+SRCS    = ft_isalnum.c ft_memccpy.c ft_memset.c ft_strncmp.c ft_strchr.c ft_isalpha.c \
 ft_strnstr.c ft_atoi.c ft_isascii.c ft_strlcat.c ft_strrchr.c ft_bzero.c \
 ft_isdigit.c ft_memcpy.c ft_strlcpy.c ft_tolower.c ft_isprint.c ft_strlen.c \
 ft_toupper.c ft_memmove.c ft_memchr.c ft_memcmp.c ft_calloc.c ft_strdup.c \
 ft_substr.c ft_strjoin.c ft_strtrim.c ft_itoa.c ft_strmapi.c ft_putchar_fd.c \
 ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_split.c ft_str_c_index.c \
 ft_putxchary_fd.c ft_putnbr_u_fd.c ft_utoa.c ft_llutoa_base.c ft_ltoa.c \
+ft_lstadd_back.c ft_lstadd_front.c ft_lstclear.c ft_lstdelone.c \
+ft_lstiter.c ft_lstlast.c ft_lstnew.c ft_lstsize.c ft_lstmap.c \
 get_next_line.c get_next_line_utils.c
 
-BONUS	= ft_lstadd_back.c ft_lstadd_front.c ft_lstclear.c ft_lstdelone.c \
-ft_lstiter.c ft_lstlast.c ft_lstnew.c ft_lstsize.c ft_lstmap.c
+OBJS    = $(SRCS:.c=.o)
 
-OBJS	= $(SRCS:.c=.o)
-
-OBJSBONUS	= $(BONUS:.c=.o)
-
-NAME	= libft.a
-CC		= gcc
-RM		= rm -f
-CFLAGS	= -Wall -Wextra -Werror
-AR		= ar -cr
+NAME    = libft.a
+CC              = gcc
+RM              = rm -f
+CFLAGS  = -Wall -Wextra -Werror
+AR              = ar -cr
 
 %.o: %.c	libft.h
 			$(CC) $(CFLAGS) -c $< -o $@
@@ -26,18 +23,14 @@ AR		= ar -cr
 $(NAME):	$(OBJS)
 			$(AR) $(NAME) $?
 
-all: 		$(NAME)
+all:	$(NAME)
 
 clean:
-			$(RM) $(OBJS)
-			$(RM) $(OBJSBONUS)
+		$(RM) $(OBJS)
 
-fclean:		clean
-			$(RM) $(NAME)
+fclean:	clean
+		$(RM) $(NAME)
 
-re:			fclean all
+re:		fclean all
 
-bonus:		$(OBJSBONUS)
-			$(AR) $(NAME) $(OBJSBONUS)
-			
-.PHONY:		bonus re fclean clean all
+.PHONY:	re fclean clean all
--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 14:28:45 by kzennoun          #+#    #+#             */
-/*   Updated: 2021/02/16 12:22:42 by kzennoun         ###   ########lyon.fr   */
+/*   Updated: 2021/02/19 13:09:12 by kzennoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,40 @@ int	gnl_bis(char **line, int nl_i, int read_ret, char **s)
 {
 	char	*temp;
 
-	if (read_ret == 0 && ((nl_i = ft_str_index_c(*s, '\n')) == -1))
+	nl_i = ft_str_index_c(*s, '\n');
+	if (read_ret == 0 && (nl_i == -1))
 	{
-		if ((*line = ft_gnl_substr(*s, ft_strlen(*s), 0, ft_strlen(*s)))
-		== NULL)
+		*line = ft_gnl_substr(*s, ft_strlen(*s), 0, ft_strlen(*s));
+		if (*line == NULL)
 			return (free_and_return_int(*s, -1));
 		free(*s);
 		*s = NULL;
 		return (0);
 	}
-	if (((temp = ft_gnl_substr(*s, ft_strlen(*s), nl_i + 1, ft_strlen(*s) -
-	nl_i - 1)) == NULL) || (((*line = ft_gnl_substr(*s, ft_strlen(*s), 0, nl_i))
-	== NULL)))
+	temp = ft_gnl_substr(*s, ft_strlen(*s), nl_i + 1, ft_strlen(*s) - nl_i - 1);
+	*line = ft_gnl_substr(*s, ft_strlen(*s), 0, nl_i);
+	if (temp == NULL || *line == NULL)
 		return (free_and_return_int(*s, -1));
 	free(*s);
 	*s = temp;
 	return (1);
+}
+
+int	gnl_checks(int fd, char **line, char **s)
+{
+	if (fd < 0 || read(fd, 0, 0) || !line)
+	{
+		if (*s)
+			return (free_and_return_int(*s, -1));
+		return (-1);
+	}
+	if (!*s)
+	{
+		*s = ft_calloc(1, sizeof(char));
+		if (!*s)
+			return (-1);
+	}
+	return (0);
 }
 
 int	get_next_line(int fd, char **line)
@@ -49,17 +67,11 @@ int	get_next_line(int fd, char **line)
 	int				nl_i;
 	char			*temp;
 
-	if (fd < 0 || read(fd, 0, 0) || !line)
-	{
-		if (s)
-			return (free_and_return_int(s, -1));
+	if (gnl_checks(fd, line, &s) == -1)
 		return (-1);
-	}
-	if (!s)
-		if (!(s = ft_calloc(1, sizeof(char))))
-			return (-1);
 	read_ret = 1;
-	while ((nl_i = ft_str_index_c(s, '\n')) == -1 && (read_ret > 0))
+	nl_i = -1;
+	while (nl_i == -1 && (read_ret > 0))
 	{
 		read_ret = read(fd, buffer, 128);
 		temp = ft_gnl_join(s, buffer, read_ret);
@@ -67,6 +79,7 @@ int	get_next_line(int fd, char **line)
 			return (free_and_return_int(s, -1));
 		free(s);
 		s = temp;
+		nl_i = ft_str_index_c(s, '\n');
 	}
 	return (gnl_bis(line, nl_i, read_ret, &s));
 }
